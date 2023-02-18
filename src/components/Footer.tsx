db254cb41a34c68cdeebdd00eda51531dd1e0f70
Alexis Provost <alexis@provost.cloud>
2023-02-17 22:10:55 -0500

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,28 +8,30 @@ const Footer = () => {
 					<ul className="footer-list">
 						<li>Copyright Alexis Provost {new Date().getFullYear()}</li>
 						<li className="spacer">|</li>
-						<li>Made with React</li>
+						<li>Made with <a href="https://reactjs.org/" target="_blank">React</a></li>
 						<li className="spacer">|</li>
-						<li>
-							<a href="https://twitter.com/_alexisprovost" target="_blank">
-								<Twitter />
-							</a>
-						</li>
-						<li>
-							<a href="https://www.linkedin.com/in/alexisprovost/" target="_blank">
-								<Linkedin />
-							</a>
-						</li>
-						<li>
-							<a href="https://github.com/alexisprovost" target="_blank">
-								<Github />
-							</a>
-						</li>
-						<li>
-							<a href="https://www.instagram.com/alexis.provost/" target="_blank">
-								<Instagram />
-							</a>
-						</li>
+						<div className="socials">
+							<li>
+								<a href="https://twitter.com/_alexisprovost" target="_blank">
+									<Twitter />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com/in/alexisprovost/" target="_blank">
+									<Linkedin />
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/alexisprovost" target="_blank">
+									<Github />
+								</a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/alexis.provost/" target="_blank">
+									<Instagram />
+								</a>
+							</li>
+						</div>
 					</ul>
 				</div>
 			</div>
