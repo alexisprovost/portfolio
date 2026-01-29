03b2b948a9a6a798cd86c93596a79ef7912cd7f4
Alexis Provost <provost21j@gmail.com>
2026-01-28 19:43:48 -0500

    fix: use svh instead of dvh for iOS Safari floating toolbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ export const HomePage = ({ locale, onLocaleChange }: HomePageProps) => {
   return (
     <div
       className={cn(
-        "min-h-screen min-h-dvh w-full",
+        "min-h-screen min-h-svh w-full",
         "flex flex-col",
         "px-5 sm:px-6 py-6 sm:py-8",
         "bg-sand [html[data-theme='dark']_&]:bg-warm-black"
